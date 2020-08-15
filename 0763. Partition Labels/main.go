e52ae060d60de074b18e39f97bca49fc21362eb3
wangwx <wangwx@tuya.com>
2020-08-15 16:26:48 +0800

    uopdate

--- 0763. Partition Labels/main.go
+++ 0763. Partition Labels/main.go
@@ -1,1 +1,33 @@
-package _763__Partition_Labels
+package main
+
+func partitionLabels(S string) []int {
+	lens := len(S)
+	end := lens - 1
+	begin := 0
+	result := make([]int, 0, 1)
+	bianli := make(map[uint8]int)
+	for begin <= end {
+		for S[begin] != S[end] {
+			if _, ok := bianli[S[end]]; !ok {
+				bianli[S[end]] = end
+			}
+			end--
+		}
+		tmpBegin := begin
+		for begin < end {
+			if value, ok := bianli[S[begin]]; ok {
+				if value > end {
+					end = value
+				}
+			}
+			begin++
+		}
+		result = append(result, end-tmpBegin+1)
+		if end == lens-1 {
+			break
+		}
+		begin = end + 1
+		end = lens - 1
+	}
+	return result
+}
